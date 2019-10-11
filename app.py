0b19142636834485b5c927a720f74c7bcee9d66b
ina-uzu <ina.uzu.song@gmail.com>
2019-10-11 14:36:54 +0900

    implement shortcut CR api

--- app.py
+++ app.py
@@ -1,34 +1,57 @@
 from flask import Flask
 from flask_restful import Resource,reqparse, Api
-from dao import userdao
+from dao import userdao, shortcutdao
 
 app = Flask(__name__)
 api = Api(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Easier Voice Assistant'
 
+
 @app.route('/user', methods=['GET'])
-def getAllUser():
-    return userdao.getAllUsers()
+def get_all_user():
+    return userdao.fina_all()
+
 
 @app.route('/user', methods=['POST'])
-def createUser():
+def create_user():
     try:
         parser = reqparse.RequestParser()
         parser.add_argument('name', required=True, type=str, help='name cannot be blank')
         args = parser.parse_args()
-        return userdao.createUser(str(args['name']))
+        return userdao.add(str(args['name']))
     except Exception as e :
         return {'error': str(e)}
 
 
-class test(Resource):
-    def post(self):
-        return {'status': 'success'}
- 
-api.add_resource(test, '/test')
+@app.route('/shortcut', methods=['GET'])
+def get_all_shortcuts():
+    try:
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', required=True, type=int, help='user_id cannot be blank')
+        args = parser.parse_args()
+        return shortcutdao.find_by_user(args['user_id'])
+
+    except Exception as e :
+        return {'error': str(e)}
+
+
+@app.route('/shortcut', methods=['POST'])
+def create_shortcut():
+    try:
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', required=True, type=int, help='user_id cannot be blank')
+        parser.add_argument('keyword', required=True, type=str, help='keyword cannot be blank')
+        parser.add_argument('command', required=True, type=str, help='command cannot be blank')
+        args = parser.parse_args()
+
+        return shortcutdao.add(int(args['user_id']), str(args['keyword']), str(args['command]']))
+
+    except Exception as e:
+        return {'error': str(e)}
 
 
 if __name__ == '__main__':
--- dao/shortcutdao.py
+++ dao/shortcutdao.py
@@ -5,32 +5,31 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(host='eva.c09iaudkjatg.ap-northeast-2.rds.amazonaws.com', port=3306, user='admin',
-                           password='evaeva2019',
-                           db='eva', charset='utf8')
+                           password='evaeva2019', db='eva', charset='utf8')
 
 
-def user_handler(obj):
+def shortcut_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 
-def add(name):
+def add(user_id, keyword, command):
     conn = get_connection()
     curs = conn.cursor()
-    ok = curs.execute("INSERT INTO user(name, create_datetime) VALUES (%s, now())", name)
+    ok = curs.execute("INSERT INTO shortcut(user_id, keyword, command, create_datetime) VALUES (%d,%s,%s, now())",
+                      user_id, keyword, command)
     conn.commit()
     conn.close()
-
     return json.dumps({'rows': ok})
 
 
-def fina_all():
+def find_by_user(user_id):
     conn = get_connection()
 
     curs = conn.cursor(pymysql.cursors.DictCursor)
-    sql = "SELECT * FROM user"
+    sql = "SELECT * FROM shortcut WHERE user_id is %d" % user_id
     curs.execute(sql)
 
     rows = curs.fetchall()
     conn.close()
 
-    return json.dumps(rows, default=user_handler)
+    return json.dumps(rows, default=shortcut_handler)
