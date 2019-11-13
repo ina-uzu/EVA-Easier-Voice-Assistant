28303bc550d1a97f3b1cf45e49d33158fbb20c59
ina-uzu <ina.uzu.song@gmail.com>
2019-11-13 20:18:34 +0900

    Merge branch 'master' of D:\Ina_Pycharm_Projects\eva with conflicts.

--- dao/userdao.py
+++ dao/userdao.py
@@ -1,10 +1,13 @@
 # -- coding: utf-8 --
+#처리된 데이터를 json형식으로 리턴해주기위해 사용
 import json
+#python mysql 연결 드라이버
 import pymysql
 
 def getConnection():
-    return pymysql.connect(host='eva.c09iaudkjatg.ap-northeast-2.rds.amazonaws.com', port=3306, user='admin', password='evaeva2019',
+    return pymysql.connect(host='localhost', port=3306, user='root', password='ina@kokoho97',
                            db='eva', charset='utf8')
+
 def user_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
