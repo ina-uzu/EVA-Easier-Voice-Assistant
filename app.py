7caaa5ebb1068414a8d036d5a8bb11c48fec7ab6
ina-uzu <ina.uzu.song@gmail.com>
2019-11-18 12:48:29 +0900

    swagger added

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import request
 from flask_restful import Resource,reqparse, Api
+from flask_restful_swagger import swagger
 from dao import userdao, shortcutdao
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 
+###################################
+# Wrap the Api with swagger.docs. It is a thin wrapper around the Api class that adds some swagger smarts
+api = swagger.docs(Api(app), apiVersion='0.1')
+###################################
 
 @app.route('/')
 def hello_world():
